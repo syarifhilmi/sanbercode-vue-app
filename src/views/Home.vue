c17eb42b74a07688e165174d8b0aaa08cad52bb3
syarifhilmi <dhonisy@gmail.com>
2021-07-03 06:25:06 +0700

    add axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,3 +3,15 @@
     <h1>This is an home page</h1>
   </div>
 </template>
+
+<script>
+export default {
+  created() {
+    const api = "http://demo-api-vue.sanbercloud.com/api/v2/blog/random/3";
+    this.axios.get(api).then((response) => {
+      console.log(response.data);
+    });
+  },
+};
+</script>
+
