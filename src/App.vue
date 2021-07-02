411281af8e2614748e94cfba191597636c5fe01f
syarifhilmi <dhonisy@gmail.com>
2021-07-03 06:07:00 +0700

    add drawer

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,82 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-navigation-drawer app v-model="drawer">
+      <v-list>
+        <v-list-item v-if="!guest">
+          <v-list-item-avatar>
+            <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title>Suparman</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+        <div class="pa-2" v-if="guest">
+          <v-btn block color="primary" class="mb-1">
+            <v-icon left>mdi-lock</v-icon>
+            Login
+          </v-btn>
+          <v-btn block color="success">
+            <v-icon left>mdi-account</v-icon>
+            Register
+          </v-btn>
+        </div>
 
-      <v-spacer></v-spacer>
+        <v-devider></v-devider>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+        <v-list-item
+          v-for="(item, index) in menus"
+          :key="`menu-${index}`"
+          :to="item.route"
+        >
+          <v-list-item-icon>
+            <v-icon left>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <template v-slot:append v-if="!guest">
+        <div class="pa-2">
+          <v-btn block color="red" dark @click="logout">
+            <v-icon left>mdi-lock</v-icon>
+            Logout
+          </v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <!-- -->
     </v-app-bar>
 
+    <!-- Sizes your content based upon application components -->
     <v-main>
-      <router-view/>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
     </v-main>
+
+    <v-footer app> @Sanbercode | VueJS </v-footer>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
   data: () => ({
-    //
+    drawer: true,
+    menus: [
+      { title: "Home", icon: "mdi-home", route: "/" },
+      { title: "Blogs", icon: "mdi-note", route: "/blogs" },
+    ],
+    guest: true,
   }),
 };
 </script>
