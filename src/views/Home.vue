1c4f8eafae8fbe4565e5ca661a1c987c7f9c22d3
syarifhilmi <dhonisy@gmail.com>
2021-07-03 09:31:21 +0700

    edit home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,59 @@
 <template>
-  <div class="about">
-    <h1>This is an home page</h1>
-  </div>
+  <v-container class="ma-0 pa-0" grid-list-sm>
+    <div class="text-right">
+      <v-btn small text to="/blogs" class="blue--text">
+        All Blogs <v-icon>mdi-chevron-right</v-icon>
+      </v-btn>
+    </div>
+
+    <v-layout wrap>
+      <v-flex v-for="blog in blogs" :key="`blog-` + blog.id" xs6>
+        <v-card :to="'/blog/' + blog.id">
+          <v-img
+            :src="
+              blog.photo
+                ? apiDomain + blog.photo
+                : 'https://picsum.photos/200/300'
+            "
+            class="white--text"
+            height="200px"
+          >
+            <v-card-title
+              class="fill-height align-end"
+              v-text="blog.title"
+            ></v-card-title>
+          </v-img>
+          <v-card-actions>
+            <v-progress-linear color="blue-grey" height="7"></v-progress-linear>
+          </v-card-actions>
+          <v-card-actions>
+            <span>{{ blog.title.substring(0, 15) }}...</span>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    apiDomain: "http://demo-api-vue.sanbercloud.com",
+    blogs: [],
+  }),
   created() {
-    const api = "http://demo-api-vue.sanbercloud.com/api/v2/blog/random/3";
-    this.axios.get(api).then((response) => {
-      console.log(response.data);
-    });
+    const config = {
+      method: "get",
+      url: this.apiDomain + "/api/v2/blog/random/4",
+    };
+    this.axios(config)
+      .then((response) => {
+        let { blogs } = response.data;
+        this.blogs = blogs;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
 };
 </script>
