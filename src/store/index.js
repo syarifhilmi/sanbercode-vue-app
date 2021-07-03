07965f123189e0b15659b438db7ee073acc019e9
syarifhilmi <dhonisy@gmail.com>
2021-07-04 06:01:32 +0700

    login5

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,16 @@ import Vuex from 'vuex'
 import alert from './alert'
 import dialog from './dialog'
 import auth from './auth'
+import VuexPersist from 'vuex-persist'
+
+const vuexPersist = new VuexPersist({
+    key: 'sanbercode',
+    storage: localStorage
+})
 
 Vue.use(Vuex)
-
 export default new Vuex.Store({
+    plugins: [vuexPersist.plugin],
     modules: {
         alert,
         dialog,
